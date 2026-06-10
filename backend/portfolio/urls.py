1e9a3034200c97e567e53d0eed242511f59d8df5
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2026-06-10 13:46:07 +0530

    Update urls.py

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapp.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    path('api/', include('myapp.urls')), # Sirf tumhari API chalegi
 ]
