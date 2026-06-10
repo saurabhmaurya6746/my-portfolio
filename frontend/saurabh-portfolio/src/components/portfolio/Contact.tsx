8772e43d2aa1345a61a7b2d91d212158cd7e1f17
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2026-06-10 13:12:48 +0530

    Update Contact.tsx

--- frontend/saurabh-portfolio/src/components/portfolio/Contact.tsx
+++ frontend/saurabh-portfolio/src/components/portfolio/Contact.tsx
@@ -34,7 +34,7 @@ export function Contact() {
 
   try {
     const response = await fetch(
-      "http://127.0.0.1:8000/api/contact/",
+      "/api/contact/",
       {
         method: "POST",
         headers: {
