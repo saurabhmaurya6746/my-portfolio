3cb086bf2983c5e2958692397ca455fe7aae2fdd
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2026-06-11 18:26:50 +0530

    Update settings.py

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -33,7 +33,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+
 ROOT_URLCONF = 'portfolio.urls'
 CORS_ALLOWED_ORIGINS=[
 "https://my-portfolio-3xdu.onrender.com"
