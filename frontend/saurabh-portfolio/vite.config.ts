5115b5231a745f2a135432cb3c0903dadd02540f
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2026-06-11 17:33:25 +0530

    Update vite.config.ts

--- frontend/saurabh-portfolio/vite.config.ts
+++ frontend/saurabh-portfolio/vite.config.ts
@@ -9,7 +9,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
-
 export default defineConfig({
   plugins: [
     react(),
